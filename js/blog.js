3bd6cc67873a27d051888776753efcb7ae32ed77
Collin Bauer <collinlbauer@gmail.com>
2021-06-19 22:00:39 -0400

    add JS functionality

--- js/blog.js
+++ js/blog.js
@@ -1,16 +1,45 @@
+
 function filterList() {
-  var selector = document.getElementById('filterList');
-  var filterText = selector.options[selector.selectedIndex].value;
-  var lis = document.querySelectorAll('.posts li');
+    var selector = document.getElementById('filterList');
+    var filterText = selector.options[selector.selectedIndex].value;
+    var lis = document.querySelectorAll('.posts li');
   
 //      console.log(filterText); //debug
-  for (var x = 0; x < lis.length; x++) {
+    for (var x = 0; x < lis.length; x++) {
 //        console.log(x, lis[x].className); //debug
-    if (filterText == 'none' || filterText == lis[x].className) {
-        lis[x].removeAttribute('hidden');
+        if (filterText == 'none' || filterText == lis[x].className) {
+            lis[x].removeAttribute('hidden');
+        }
+        else {
+            lis[x].setAttribute('hidden', true);
+        }
     }
-    else {
-        lis[x].setAttribute('hidden', true);
-    }
-  }
 }
+
+
+function onStart() {
+    // get query information
+    const queryString = window.location.search;
+    //const queryString = "?filter=shmoe" // for testing in node
+    const urlParams = new URLSearchParams(queryString);
+
+
+    // set initial filter, if applicable
+    const initialFilter = urlParams.get('filter')
+    //console.log(initialFilter); //debug
+
+    if (initialFilter != null) {
+        var selector = document.getElementById('filterList');
+        var i = 0;
+        while ( i < selector.options.length) {
+            if (selector.options[i].value == initialFilter) {
+                selector.selectedIndex = i;
+                filterList();
+                return;
+            }
+            i++;
+        }
+    }
+}
+
+onStart()
